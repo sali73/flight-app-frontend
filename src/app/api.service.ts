5d8cbdd7cb8ec2050bbbe9bebf69e5dc87b4aab0
sali73 <sally.elgendy@hotmail.com>
2020-10-02 14:54:08 -0400

    first commit

---
+++ src/app/api.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  baseurl = ' http://127.0.0.1:8000';
+  httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json'});
+
+  constructor( private  http: HttpClient) { }
+
+  getAllFlight(): Observable<any>{
+    return this.http.get(this.baseurl + '/flight/' , {headers: this.httpHeaders});
+  }
+
+  getOneFlight(id): Observable<any>{
+    return this.http.get(this.baseurl + '/flight/' + id + '/' , {headers: this.httpHeaders});
+  }
+
+   updateFlight(flight): Observable<any>{
+    const body = {
+      flight: flight.flight ,  date: flight.date, destination: flight.destination ,
+      scheduled_departure_time: flight.scheduled_departure_time , assigned_plane_type: flight.assigned_plane_type ,
+      capacity: flight.capacity , seats_reserved: flight.seats_reserved , seats_available: flight.seats_available };
+
+    return this.http.put(this.baseurl + '/flight/' + flight.id + '/' , body , {headers: this.httpHeaders});
+  }
+  createFlight(flight): Observable<any>{
+    const body = {
+      flight: flight.flight ,  date: flight.date, destination: flight.destination ,
+      scheduled_departure_time: flight.scheduled_departure_time , assigned_plane_type: flight.assigned_plane_type ,
+      capacity: flight.capacity , seats_reserved: flight.seats_reserved , seats_available: flight.seats_available };
+
+    return this.http.post(this.baseurl + '/flight/', body , {headers: this.httpHeaders});
+  }
+
+
+}
+
---
+++ src/app/app.component.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+import {ApiService} from './api.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [ApiService]
+})
+export class AppComponent {
+  flights = [{flight: 'test'}];
+  selectedFlight;
+
+  constructor(private  api: ApiService) {
+    this.getFlights();
+    this.selectedFlight = { id: -1 , flight: '' ,  date: '', destination: '', scheduled_departure_time : '' ,
+    assigned_plane_type: '', capacity: '' , seats_reserved: '' , seats_available: '' };
+  }
+  getFlights = () => {
+    this.api.getAllFlight().subscribe(
+      data => {
+        this.flights = data.results;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+  flightClicked = (flight) => {
+    // console.log( flight.id)
+    this.api.getOneFlight(flight.id).subscribe(
+      data => {
+        this.selectedFlight = data;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+  updateFlight = () => {
+        this.api.updateFlight(this.selectedFlight).subscribe(
+      data => {
+        this.selectedFlight = data;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+  createFlight = () => {
+        this.api.createFlight(this.selectedFlight).subscribe(
+      data => {
+        this.flights.push(data);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+}
+
